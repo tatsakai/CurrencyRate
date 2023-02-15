f081d6f038b723d9fb13198acf672eef46a0b947
tatsakai <116135281+tatsakai@users.noreply.github.com>
2023-02-15 10:24:38 +0900

    modified:   Currency.cs

--- Currency.cs
+++ Currency.cs
@@ -44,42 +44,42 @@ namespace Contoso.CurrencyExchange
             string ToCurrency   = req.Query["TO"];
                      
             HttpClient apiclient = new HttpClient();
-            string rtnval = "0.0";
-            // string Rate ="";
-            // string selrate ="";
-            // try
-            // {
-            //     string requestBody = await apiclient.GetStringAsync("https://www.gaitameonline.com/rateaj/getrate");
-            //     CurrencyRale data = JsonSerializer.Deserialize<CurrencyRale>(requestBody);
-            //     //comment
-            //     if (data != null)
-            //     {
-            //         if(data.quotes.Count > 0) 
-            //         { 
-            //             Rate = FromCurrency + ToCurrency;
-            //             selrate = data.quotes.Where(q => q.currencyPairCode.Equals(Rate)).FirstOrDefault().ask;
-            //             if (selrate == null)
-            //             {
-            //                 Rate = ToCurrency + FromCurrency;
-            //                 selrate = data.quotes.Where(q => q.currencyPairCode.Equals(Rate)).FirstOrDefault().ask;
-            //                 if (selrate != null)
-            //                 {
-            //                     double calrate = 1 / Convert.ToDouble(selrate);
-            //                     rtnval = calrate.ToString("F5");
-            //                 }
-            //             }
-            //             else
-            //             {
-            //                 rtnval = selrate;
-            //             }
-            //         }
-            //     }
-            // }
-            // catch(Exception e)
-            // {
-            //     rtnval = "0.0";
-            //     log.LogError(e.Message);
-            // }
+            string rtnval = "0.000";
+            string Rate ="";
+            string selrate ="";
+            try
+            {
+                string requestBody = await apiclient.GetStringAsync("https://www.gaitameonline.com/rateaj/getrate");
+                CurrencyRale data = JsonSerializer.Deserialize<CurrencyRale>(requestBody);
+                //comment
+                if (data != null)
+                {
+                    if(data.quotes.Count > 0) 
+                    { 
+                        Rate = FromCurrency + ToCurrency;
+                        selrate = data.quotes.Where(q => q.currencyPairCode.Equals(Rate)).FirstOrDefault().ask;
+                        if (selrate == null)
+                        {
+                            Rate = ToCurrency + FromCurrency;
+                            selrate = data.quotes.Where(q => q.currencyPairCode.Equals(Rate)).FirstOrDefault().ask;
+                            if (selrate != null)
+                            {
+                                double calrate = 1 / Convert.ToDouble(selrate);
+                                rtnval = calrate.ToString("F5");
+                            }
+                        }
+                        else
+                        {
+                            rtnval = selrate;
+                        }
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                rtnval = "0.0";
+                log.LogError(e.Message);
+            }
 
             return new OkObjectResult(new FunctionResult {Rate = rtnval});
         }
